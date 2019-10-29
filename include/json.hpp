2453cf474677a5b56e6d0d0374122fbdaa8047fc
DM00n <54860965+DM00n@users.noreply.github.com>
2019-10-29 13:26:24 +0300

    v2

--- include/json.hpp
+++ include/json.hpp
@@ -7,38 +7,49 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+
 class Json {
 private:
-    std::map <std::string,std::any> _json_map;
+    std::map <std::string, std::any> _json_map;
     std::vector <std::any> _json_vector;
     std::string _json_string;
 public:
     void print();
+
     int detect_type(unsigned j) const;
+
     bool is_letter(unsigned j) const;
+
     bool is_number(unsigned j) const;
+
     bool is_bool(char s, int j) const;
-    bool is_empty (char s) const;
-    Json(const std::string&s);
+
+    bool is_empty(char s) const;
+
+    Json(const std::string &s);
+
     Json();
+
     bool is_array() const;
+
     bool is_object() const;
 
     // Метод возвращает значение по ключу key, если экземпляр является JSON-объектом.
     // Значение может иметь один из следующих типов: Json, std::string, double, bool или быть пустым.
     // Если экземпляр является JSON-массивом, генерируется исключение.
-    std::any& operator[](const std::string& key);
+    std::any &operator[](const std::string &key);
 
     // Метод возвращает значение по индексу index, если экземпляр является JSON-массивом.
     // Значение может иметь один из следующих типов: Json, std::string, double, bool или быть пустым.
     // Если экземпляр является JSON-объектом, генерируется исключение.
-    std::any& operator[](int index);
+    std::any &operator[](int index);
 
     // Метод возвращает объект класса Json из строки, содержащей Json-данные.
-    Json parse(const std::string& s);
+    Json parse(const std::string &s);
 
     // Метод возвращает объекта класса Json из файла, содержащего Json-данные в текстовом формате.
-    Json parseFile(const std::string& path_to_file);
+    Json parseFile(const std::string &path_to_file);
 
 };
+
 #endif
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,22 +1,21 @@
-#include "json.h"
+#include "json.hpp"
 #include <iostream>
+
 int main() {
     std::string str1;
     std::ifstream in;
     std::string line;
     in.open(R"(D:\project\3sem_parserJSON\test.txt)");
-    if (!in)
-    {
-        std::cout <<"error opening file";
+    if (!in) {
+        std::cout << "error opening file";
         return 0;
     }
-    while(getline(in, line))
-    {
-        str1 +=line+'\n';
+    while (getline(in, line)) {
+        str1 += line + '\n';
     }
     in.close();
-Json instance(str1);
-instance.parseFile(R"(D:\project\3sem_parserJSON\test.txt)");
+    Json instance(str1);
+    instance.parseFile(R"(D:\project\3sem_parserJSON\test.txt)");
 
 //instance.print();
     return 0;
