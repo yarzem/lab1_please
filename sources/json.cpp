aa0d09af970e7c618fc78a26fba17b51e2de4724
DM00n <54860965+DM00n@users.noreply.github.com>
2019-11-02 13:44:28 +0300

    for travis

--- sources/json.cpp
+++ sources/json.cpp
@@ -299,8 +299,9 @@ bool Json::is_object() const {
                 while (j < _json_string.length()) {
                     if (is_empty(_json_string[j])){
                         j++;
-                    } else
+                    } else {
                         break;
+                    }
                 }
                 char ch = _json_string[j];
                 unsigned k = j + 1;
