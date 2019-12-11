b9c936fa1009fe62ff0a3f57dd9d698c10ff5426
Дмитрий Астахов <teamvortex@yandex.ru>
2019-12-11 12:32:31 +0300

    test war

--- sources/json.cpp
+++ sources/json.cpp
@@ -1,23 +1,23 @@
 // Copyright 2019 DM00n <teamvortex@yandex.ru>
-#include "json.hpp"
+#include "json.h"
 
 bool Json::is_empty(char s) const {
     return (s == ' ') || (s == '\n') || (s == '\t');
 }
 
-bool Json::is_bool(char s, int j) const {
-    if (s == 'f') {
+bool Json::is_bool(std::string s, char c, int j) const {
+    if (c == 'f') {
         char ex[6];
         for (int i = 0; i < 5; i++) {
-            ex[i] = _json_string[j + i];
+            ex[i] = s[j + i];
         }
         ex[5] = '\0';
         return strcmp(ex, "false") == 0;
     }
-    if (s == 't') {
+    if (c == 't') {
         char ex[5];
         for (int i = 0; i < 4; ++i) {
-            ex[i] = _json_string[j + i];
+            ex[i] = s[j + i];
         }
         ex[4] = '\0';
         return strcmp(ex, "true") == 0;
@@ -25,353 +25,42 @@ bool Json::is_bool(char s, int j) const {
     return false;
 }
 
-bool Json::is_letter(unsigned j) const {
-    return ((_json_string[j] >= 'a') && (_json_string[j] <= 'z')) ||
-           ((_json_string[j] >= 'A') && (_json_string[j] <= 'Z'));
+bool Json::is_letter(std::string s, unsigned j) const {
+    return ((s[j] >= 'a') && (s[j] <= 'z')) ||
+           ((s[j] >= 'A') && (s[j] <= 'Z'));
 }
 
-bool Json::is_number(unsigned j) const {
-    return (_json_string[j] >= '0') && (_json_string[j] <= '9');
+bool Json::is_number(std::string s, unsigned j) const {
+    return (s[j] >= '0') && (s[j] <= '9');
 }
 
-int Json::detect_type(unsigned j) const {
-    char ch = _json_string[j];
+int Json::detect_type(std::string s, unsigned j) const {
+    char ch = s[j];
     if (ch == 'f') {
-        if (is_bool('f', j)) return 0;
+        if (is_bool(s, 'f', j)) return 0;
         return 5;
     }
     if (ch == 't') {
-        if (is_bool('t', j)) return 0;
+        if (is_bool(s, 't', j)) return 0;
         return 5;
     }
     if (ch == '\"') return 1;
     if (ch == '[') return 2;
     if (ch == '{') return 3;
-    if (is_number(j)) return 4;
+    if (is_number(s, j)) return 4;
     return 5;
 }
 
-Json::Json(const std::string &s) { _json_string = s; }
+Json::Json(const std::string &s) { parse(s); }
 
 bool Json::is_array() const {
-    char symbol;
-    int l_square_brace = 0,
-            r_square_brace = 0,
-            l_figure_brace = 0,
-            r_figure_brace = 0,
-            mas_type = -1,
-            quotes = 0,
-            skip_step = 0;
-    for (unsigned i = 0; i < _json_string.length(); ++i) {
-        symbol = _json_string[i];
-        if (skip_step != 0) {
-            skip_step--;
-            continue;
-        }
-        if (!is_empty(symbol)) {
-            if ((symbol == '{') && (quotes % 2 == 0)) l_figure_brace++;
-            if ((symbol == '}') && (quotes % 2 == 0)) r_figure_brace++;
-            if ((symbol == '[') && (quotes % 2 == 0)) l_square_brace++;
-            if ((symbol == ']') && (quotes % 2 == 0)) {
-                r_square_brace++;
-                if (l_square_brace - r_square_brace == 0) mas_type = -1;
-            }
-            if (symbol == '\"') quotes++;
-            if (((l_figure_brace == 0)
-                 && (symbol != '{'))
-                && ((l_square_brace == 0)
-                    && (symbol != '[')))
-                return false;
-            if ((symbol == '[') && (l_square_brace == 1) &&
-                (mas_type = -1)) {
-                unsigned j = i + 1;
-                while ((j <= _json_string.length())
-                       && (is_empty(_json_string[j]))) { j++; }
-                mas_type = detect_type(j);
-                continue;
-            }
-            if (symbol == ']') {
-                unsigned j = i + 1;
-                while ((j <= _json_string.length())
-                       && (is_empty(_json_string[j]))) { j++; }
-                if (_json_string[j] == '[') return false;
-            }
-            if (symbol != ',') {
-                switch (mas_type) {
-                    case 0 : {
-                        if ((symbol != 'f') && (symbol != 't')) return false;
-                        if (symbol == 'f') {
-                            if (is_bool('f', i) == 0) return false;
-                            unsigned j = i + 5;
-                            while ((j < _json_string.length())
-                                   && (is_empty(_json_string[j]))) { j++; }
-                            if ((_json_string[j] != ',')
-                                && (_json_string[j] != ']'))
-                                return false;
-                            skip_step = 4;
-                        }
-                        if (symbol == 't') {
-                            if (is_bool('t', i) == 0) return false;
-                            unsigned j = i + 4;
-                            while ((j < _json_string.length())
-                                   && (is_empty(_json_string[j]))) { j++; }
-                            if ((_json_string[j] != ',')
-                                && (_json_string[j] != ']'))
-                                return false;
-                            skip_step = 3;
-                        }
-
-                        break;
-                    }
-                    case 1 : {
-                        if ((quotes % 2 == 0)
-                            && (!((symbol == ',')
-                                  || (symbol == ']')
-                                  || (symbol == '\"'))))
-                            return false;
-                        if (quotes % 2 == 0) {
-                            unsigned j = i + 1;
-                            while ((j < _json_string.length())
-                                   && (is_empty(_json_string[j]))) { j++; }
-                            if ((_json_string[j] != ',')
-                                && (_json_string[j] != ']'))
-                                return false;
-                        }
-                        break;
-                    }
-                    case 2 : {
-                        if ((symbol != '[') && (symbol != ']')) return false;
-                        if ((symbol == '[') && (quotes % 2 == 0)) {
-                            std::string little_arr = "[";
-                            int c = 1, j = i + 1;
-                            while (c) {
-                                if (_json_string[j] == '[') c++;
-                                if (_json_string[j] == ']') {
-                                    c--;
-                                    little_arr += "]";
-                                    continue;
-                                }
-                                little_arr += _json_string[j];
-                                j++;
-                                skip_step++;
-                            }
-                            Json l_array(little_arr);
-                            if (!l_array.is_array())return false;
-                            continue;
-                        }
-                        break;
-                    }
-                    case 3 : {
-                        if ((symbol != '{') && (symbol != '}')) return false;
-                        if (symbol == '{') {
-                            std::string little_obj = "{";
-                            int c = 1, j = i + 1;
-                            while (c) {
-                                if (_json_string[j] == '{') c++;
-                                if (_json_string[j] == '}') {
-                                    c--;
-                                    little_obj += "}";
-                                    continue;
-                                }
-                                little_obj += _json_string[j];
-                                j++;
-                                skip_step++;
-                            }
-                            Json l_object(little_obj);
-                            if (!l_object.is_object())return false;
-                        }
-                        if (symbol == '}') {
-                            unsigned j = i + 1;
-                            while ((j < _json_string.length())
-                                   && (is_empty(_json_string[j]))) { j++; }
-                            if ((_json_string[j] != ',')
-                                && (_json_string[j] != ']'))
-                                return false;
-                        }
-                        break;
-                    }
-                    case 4 : {
-                        if (!is_number(i)) return false;
-                        unsigned j = i + 1;
-                        if (is_number(j)) break;
-                        if (is_empty(_json_string[j])) {
-                            while ((j <= _json_string.length())
-                                   && (is_empty(_json_string[j]))) { j++; }
-                            if ((_json_string[j] != ',')
-                                && (_json_string[j] != ']'))
-                                return false;
-                        }
-                        break;
-                    }
-                    case 5 : {
-                        return false;
-                    }
-                }
-            } else {
-                unsigned j = i + 1;
-                while ((j < _json_string.length())
-                       && (is_empty(_json_string[j]))) { j++; }
-                if (_json_string[j] == ']') return false;
-            }
-            continue;
-        }
-    }
-    return !((l_square_brace + r_square_brace == 0)
-             || (quotes % 2 == 1)
-             || (l_square_brace > r_square_brace)
-             || (l_figure_brace > r_figure_brace));
+    if (this->_json_vector.empty()) return false;
+    return true;
 }
 
 bool Json::is_object() const {
-    int l_figure_brace = 0,
-            r_figure_brace = 0,
-            l_square_brace = 0,
-            r_square_brace = 0,
-            quotes = 0,
-            skip_step = 0;
-    char symbol;
-    for (unsigned i = 0; i < _json_string.length(); ++i) {
-        symbol = _json_string[i];
-        if (skip_step != 0) {
-            skip_step--;
-            continue;
-        }
-        if (!is_empty(symbol)) {
-            if ((symbol == '{') && (quotes % 2 == 0)) l_figure_brace++;
-            if ((symbol == '}') && (quotes % 2 == 0)) r_figure_brace++;
-            if ((symbol == '[') && (quotes % 2 == 0)) l_square_brace++;
-            if ((symbol == ']') && (quotes % 2 == 0)) r_square_brace++;
-            if (symbol == '\"') quotes++;
-            if (((l_figure_brace == 0)
-                 && (symbol != '{'))
-                && ((l_square_brace == 0)
-                    && (symbol != '[')))
-                return false;
-            if (symbol == '{') {
-                unsigned j = i + 1;
-                while (j < _json_string.length()) {
-                    if (is_empty(_json_string[j])) { j++; }
-                    else
-                        break;
-                }
-                if (_json_string[j] == '{') return false;
-            }
-            if ((symbol == '{')
-                && (quotes % 2 == 0)
-                && (l_figure_brace != 1)) {
-                std::string little_obj = "{";
-                int c = 1, j = i + 1;
-                while (c) {
-                    if (_json_string[j] == '{') c++;
-                    if (_json_string[j] == '}') {
-                        c--;
-                        little_obj += "}";
-                        continue;
-                    }
-                    little_obj += _json_string[j];
-                    j++;
-                    skip_step++;
-                }
-                Json l_object(little_obj);
-                if (!l_object.is_object())return false;
-                continue;
-            }
-            if ((symbol == '[') && (quotes % 2 == 0)) {
-                std::string little_arr = "[";
-                int c = 1, j = i + 1;
-                while (c) {
-                    if (_json_string[j] == '[') c++;
-                    if (_json_string[j] == ']') {
-                        c--;
-                        little_arr += "]";
-                        continue;
-                    }
-                    little_arr += _json_string[j];
-                    j++;
-                    skip_step++;
-                }
-                Json l_array(little_arr);
-                if (!l_array.is_array())return false;
-                continue;
-            }
-            if ((quotes % 2 == 0) && (symbol == ':')) {
-                unsigned j = i + 1;
-                while (j < _json_string.length()) {
-                    if (is_empty(_json_string[j])){
-                        j++;
-                    } else {
-                        break;
-                    }
-                }
-                char ch = _json_string[j];
-                unsigned k = j + 1;
-                if (ch == '\"') {
-                    while ((k + 1 < _json_string.length())
-                           && (_json_string[k] != '\"')) {
-                        k++;
-                    }
-                    while (k + 1 < _json_string.length()) {
-                        if (is_empty(_json_string[k + 1])) { k++; }
-                        else
-                            break;
-                    }
-                    if ((_json_string[k + 1] != ',')
-                        && (_json_string[k + 1] != ']')
-                        && (_json_string[k + 1] != '}'))
-                        return false;
-                }
-                if ((ch != '\"') && (ch != '{') && (ch != '[')) {
-                    if ((is_number(j))
-                        && !((is_number(j + 1))
-                             || (ch == ',')))
-                        return false;
-                }
-                continue;
-            }
-            if ((quotes % 2 == 0) && (symbol == ',')) {
-                unsigned j = i + 1;
-                while ((j < _json_string.length())
-                       && (is_empty(_json_string[j]))) { j++; }
-                if (_json_string[j] != '\"') return false;
-                continue;
-            }
-            if ((l_figure_brace > r_figure_brace)
-                && (quotes % 2 == 0)
-                && ((symbol == '}')
-                    || (symbol == ']'))) {
-                unsigned j = i + 1;
-                while ((j < _json_string.length())
-                       && (is_empty(_json_string[j]))) { j++; }
-                if ((_json_string[j] != '}')
-                    && (_json_string[j] != ']')
-                    && (_json_string[j] != ','))
-                    return false;
-            }
-            if ((quotes % 2 == 0) && (is_letter(i))) {
-                if (symbol == 'f') {
-                    if (is_bool('f', i) == 0) return false;
-                    skip_step = 4;
-                }
-                if (symbol == 't') {
-                    if (is_bool('t', i) == 0) return false;
-                    skip_step = 3;
-                }
-                continue;
-            }
-            if ((quotes % 2 == 0) && ((symbol != '}')
-                                      && (symbol != '{')
-                                      && (symbol != '"')
-                                      && (!is_number(i))
-                                      && (symbol != '[')
-                                      && (symbol != ']')))
-                return false;
-        }
-    }
-    return !((l_figure_brace - r_figure_brace != 0)
-             || (l_figure_brace + r_figure_brace == 0)
-             || (quotes % 2 == 1)
-             || (l_square_brace > r_square_brace)
-             || (l_figure_brace > r_figure_brace));
+    if (this->_json_map.empty()) return false;
+    return true;
 }
 
 Json Json::parse(const std::string &s) {
@@ -403,7 +92,7 @@ Json Json::parse(const std::string &s) {
                         else
                             break;
                     }
-                    type = detect_type(j);
+                    type = detect_type(s, j);
                     if (type == 0) {
                         if (s[j] == 'f') {
                             value = false;
@@ -443,7 +132,7 @@ Json Json::parse(const std::string &s) {
                         }
                         skip_step = k - i;
                         Json l_array(little_arr_string);
-                        l_array = l_array.parse(little_arr_string);
+                        //l_array = l_array.parse(little_arr_string);
                         value = l_array;
                         continue;
                     }
@@ -462,7 +151,7 @@ Json Json::parse(const std::string &s) {
                         }
                         skip_step = k - i + 1;
                         Json l_obj(little_obj_string);
-                        l_obj = l_obj.parse(little_obj_string);
+                        //l_obj = l_obj.parse(little_obj_string);
                         value = l_obj;
                         continue;
                     }
@@ -545,7 +234,7 @@ Json Json::parse(const std::string &s) {
                     }
                     skip_step = k - i;
                     Json l_array(little_arr_string);
-                    l_array = l_array.parse(little_arr_string);
+                    //l_array = l_array.parse(little_arr_string);
                     value = l_array;
                     continue;
                 }
@@ -564,7 +253,7 @@ Json Json::parse(const std::string &s) {
                     }
                     skip_step = k - i;
                     Json l_arr(little_arr_string);
-                    l_arr = l_arr.parse(little_arr_string);
+                    //l_arr = l_arr.parse(little_arr_string);
                     value = l_arr;
                     continue;
                 }
@@ -587,7 +276,7 @@ Json Json::parse(const std::string &s) {
                         else
                             break;
                     }
-                    type = detect_type(j);//0-bool, 1-str, 2-mas, 3-obj, 4-int
+                    type = detect_type(s, j);//0-bool, 1-str, 2-mas, 3-obj, 4-int
                     continue;
                 }
             }
@@ -649,14 +338,21 @@ void Json::print() {
 }
 
 std::any &Json::operator[](const std::string &key) {
-    std::map<std::string, std::any>::iterator it = _json_map.find(key);
-    if (_json_vector.empty()) throw;
-    return it->second;
+    if (_json_vector.empty())
+        return _json_map.find(key)->second;
+    else
+    {
+        throw "Error: exemplar is array";
+    }
 }
 
 std::any &Json::operator[](int index) {
-    if (!_json_map.empty()) throw;
-    return _json_vector[index];
+    if (_json_vector.empty())
+    {
+        throw "Error: exemplar is object";
+    } else {
+        return _json_vector[index];
+    }
 }
 
 Json::Json() = default;
