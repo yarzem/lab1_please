08b61a5ca6abb46cd1f498829de0eb5648e682f1
Дмитрий Астахов <teamvortex@yandex.ru>
2019-12-18 17:58:39 +0300

    lvl 3 try 4

--- sources/n_json.cpp
+++ sources/n_json.cpp
@@ -5,22 +5,20 @@
 
 using json = nlohmann::json;
 
-json magic(json obj) {
+json changer(json obj) {
     json rez = json::array({});
-    for (int i = 0; i < obj[0].size(); ++i) {
+    for (unsigned i = 0; i < obj[0].size(); ++i) {
         json tmp = json::object({});
-        for (int j = 0; j < obj.size(); ++j) {
+        for (unsigned j = 0; j < obj.size(); ++j) {
             switch (j) {
                 case 0:
                     tmp.push_back(json::object_t::
                                   value_type("ticker", obj[j][i]));
                     break;
-
                 case 1:
                     tmp.push_back(json::object_t::
                                   value_type("id", obj[j][i]));
                     break;
-
                 case 2:
                     tmp.push_back(json::object_t::
                                   value_type("desciption", obj[j][i]));
